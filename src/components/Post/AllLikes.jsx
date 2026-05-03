4148e7f1bce4aaf71b6e8b4f52b64e23733cf048
Ahmed-adel23 <ahmedadelgxhero@gmail.com>
2026-05-03 17:08:24 +0200

    update likes

--- src/components/Post/AllLikes.jsx
+++ src/components/Post/AllLikes.jsx
@@ -5,7 +5,7 @@ export default function LikesModal({ isOpen, onClose, likes, isLoading }) {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 z-100 flex items-center justify-center p-4">
+    <div className="fixed inset-0 jj z-100 flex items-center justify-center p-4">
       <div
         className="fixed inset-0 bg-black/40 backdrop-blur-sm"
         onClick={onClose}
